79dcf376454d52c6faf46b99fb8d6c95ac517b10
Arpit10-web <harpitjain0207@gmail.com>
2023-02-09 09:39:08 +0530

    assignmet upload to hidden folder

---
+++ Q5.py
@@ -0,0 +1,13 @@
+try :
+    a=int(input())
+    b=0
+    d=a/b
+
+    c=int(input())
+except Exception:
+    if d==float('INF'):
+        print(" division not possilbe")
+    elif ValueError:
+        print(" ente the correct data type value ")
+    else :
+        print(" program run succesfull")
